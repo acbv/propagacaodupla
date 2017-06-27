68e0578d18a5f8ac9fc6986a9c395437735fe5a6
acbv <acbv@cin.ufpe.br>
2017-06-27 02:17:09 -0300

    Método propagação dupla e dependências do StanfordCoreNLP

---
+++ Propagacao_Dupla/src/main/java/com/acbv/propagacao_dupla/entidades/Corpus.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acbv.propagacao_dupla.entidades;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ *
+ * @author arthur
+ */
+public class Corpus {
+    
+    public List<Resenha> resenhas;
+    public HashMap<String, Integer> bagOfWords;
+    public HashMap<String, Integer> bagOf2Grams;
+    public HashMap<String, Integer> bagOf3Grams;
+    public HashMap<String, Integer> bagOf4Grams;
+    public HashMap<String, Integer> bagOf5Grams;
+
+    public Corpus(List<Resenha> resenhas) {
+        this.resenhas = resenhas;
+    }
+    
+}
---
+++ Propagacao_Dupla/src/main/java/com/acbv/propagacao_dupla/utils/Avaliacao.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.acbv.propagacao_dupla.utils;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ *
+ * @author arthur
+ */
+public class Avaliacao {
+    
+    public double recall, precision, f_measure;
+
+    public Avaliacao(double recall, double precision, double f_measure) {
+        this.recall = recall;
+        this.precision = precision;
+        this.f_measure = f_measure;
+    }
+
+    @Override
+    public String toString() {
+        return "Avaliacao{" + "recall=" + recall + ", precision=" + precision + ", f_measure=" + f_measure + '}';
+    }
+    
+    public Avaliacao getAvalicao(Set relevant, Set retrieved){
+        double n_doc_ret = retrieved.size();
+        double n_doc_rel = relevant.size();
+        List rel_ret = new ArrayList<>(relevant);
+        rel_ret.retainAll(retrieved);
+        double n_doc_rel_ret = rel_ret.size();
+        this.precision = n_doc_rel_ret/n_doc_ret;
+        this.recall = n_doc_rel_ret/n_doc_rel;
+        this.f_measure = 2 * ((precision * recall) / (precision + recall));
+        return this;
+    }
+    
+    
+}
